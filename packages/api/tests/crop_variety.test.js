c5d0e4943c3aada2c285b96b1c629406be2bc948
Shang Heng <shanghengmak@gmail.com>
2021-05-19 11:42:50 -0700

    Add test

--- packages/api/tests/crop_variety.test.js
+++ packages/api/tests/crop_variety.test.js
@@ -24,7 +24,6 @@ jest.mock('../src/middleware/acl/checkJwt');
 const mocks = require('./mock.factories');
 const { tableCleanup } = require('./testEnvironment');
 const cropVarietyModel = require('../src/models/cropVarietyModel');
-const { test } = require('../knexfile');
 
 describe('CropVariety Tests', () => {
   let middleware;
@@ -129,14 +128,6 @@ describe('CropVariety Tests', () => {
         getRequest(`/crop_variety/farm/${farm.farm_id}`, { user_id: worker.user_id }, (err, res) => {
           expect(res.status).toBe(200);
           expect(res.body[0].crop_variety_id).toBe(cropVariety.crop_variety_id);
-          done();
-        });
-      });
-
-      xtest('Workers should get seeded cropVarietys', async (done) => {
-        getRequest(`/crop_variety/farm/${farm.farm_id}`, { user_id: worker.user_id }, (err, res) => {
-          expect(res.status).toBe(200);
-          expect(res.body[1].crop_variety_id).toBe(seededCrop.crop_variety_id);
           done();
         });
       });
@@ -443,6 +434,25 @@ describe('CropVariety Tests', () => {
         const cropVarietys = await cropVarietyModel.query().where('farm_id', farm.farm_id);
         expect(cropVarietys.length).toBe(1);
         expect(cropVarietys[0].crop_variety_name).toBe(cropVariety.crop_variety_name);
+        expect(cropVarietys[0].nutrient_credits).toBe(crop.nutrient_credits);
+        done();
+      });
+    });
+
+    test('should post and get a valid cropVariety with null compliance info', async (done) => {
+      let cropVariety = fakeCropVariety(crop.crop_id);
+      cropVariety.crop_variety_name = `${cropVariety.cropVariety_specie} - ${cropVariety.crop_variety_name}`;
+      cropVariety.compliance_file_url = '';
+      cropVariety.organic = null;
+      cropVariety.treated = null;
+      cropVariety.genetically_engineered = null;
+      cropVariety.searched = null;
+      postCropVarietyRequest(cropVariety, {}, async (err, res) => {
+        expect(res.status).toBe(201);
+        const cropVarietys = await cropVarietyModel.query().where('farm_id', farm.farm_id);
+        expect(cropVarietys.length).toBe(1);
+        expect(cropVarietys[0].crop_variety_name).toBe(cropVariety.crop_variety_name);
+        expect(cropVarietys[0].organic).toBe(null);
         done();
       });
     });
